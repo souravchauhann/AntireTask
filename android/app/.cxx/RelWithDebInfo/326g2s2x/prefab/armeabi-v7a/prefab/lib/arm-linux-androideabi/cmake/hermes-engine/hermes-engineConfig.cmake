fb3eabb2c2e872ba27381098303a45dc95f1550f
Sourav <sourav@192.168.1.11>
2024-12-31 22:38:53 +0530

    Attach Apk in Readme File

---
+++ android/app/.cxx/RelWithDebInfo/326g2s2x/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "/Users/sourav/.gradle/caches/8.10.2/transforms/8be93bdf47fee54855c5fbf0680faee1/transformed/hermes-android-0.76.5-release/prefab/modules/libhermes/libs/android.armeabi-v7a/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/sourav/.gradle/caches/8.10.2/transforms/8be93bdf47fee54855c5fbf0680faee1/transformed/hermes-android-0.76.5-release/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
---
+++ android/app/.cxx/RelWithDebInfo/326g2s2x/prefab/x86/prefab/lib/i686-linux-android/cmake/ReactAndroid/ReactAndroidConfig.cmake
@@ -0,0 +1,36 @@
+if(NOT TARGET ReactAndroid::hermestooling)
+add_library(ReactAndroid::hermestooling SHARED IMPORTED)
+set_target_properties(ReactAndroid::hermestooling PROPERTIES
+    IMPORTED_LOCATION "/Users/sourav/.gradle/caches/8.10.2/transforms/f5f2c5a1aac81be5c2065c1af7ab67cb/transformed/react-android-0.76.5-release/prefab/modules/hermestooling/libs/android.x86/libhermestooling.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/sourav/.gradle/caches/8.10.2/transforms/f5f2c5a1aac81be5c2065c1af7ab67cb/transformed/react-android-0.76.5-release/prefab/modules/hermestooling/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
+if(NOT TARGET ReactAndroid::jsctooling)
+add_library(ReactAndroid::jsctooling SHARED IMPORTED)
+set_target_properties(ReactAndroid::jsctooling PROPERTIES
+    IMPORTED_LOCATION "/Users/sourav/.gradle/caches/8.10.2/transforms/f5f2c5a1aac81be5c2065c1af7ab67cb/transformed/react-android-0.76.5-release/prefab/modules/jsctooling/libs/android.x86/libjsctooling.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/sourav/.gradle/caches/8.10.2/transforms/f5f2c5a1aac81be5c2065c1af7ab67cb/transformed/react-android-0.76.5-release/prefab/modules/jsctooling/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
+if(NOT TARGET ReactAndroid::jsi)
+add_library(ReactAndroid::jsi SHARED IMPORTED)
+set_target_properties(ReactAndroid::jsi PROPERTIES
+    IMPORTED_LOCATION "/Users/sourav/.gradle/caches/8.10.2/transforms/f5f2c5a1aac81be5c2065c1af7ab67cb/transformed/react-android-0.76.5-release/prefab/modules/jsi/libs/android.x86/libjsi.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/sourav/.gradle/caches/8.10.2/transforms/f5f2c5a1aac81be5c2065c1af7ab67cb/transformed/react-android-0.76.5-release/prefab/modules/jsi/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
+if(NOT TARGET ReactAndroid::reactnative)
+add_library(ReactAndroid::reactnative SHARED IMPORTED)
+set_target_properties(ReactAndroid::reactnative PROPERTIES
+    IMPORTED_LOCATION "/Users/sourav/.gradle/caches/8.10.2/transforms/f5f2c5a1aac81be5c2065c1af7ab67cb/transformed/react-android-0.76.5-release/prefab/modules/reactnative/libs/android.x86/libreactnative.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/sourav/.gradle/caches/8.10.2/transforms/f5f2c5a1aac81be5c2065c1af7ab67cb/transformed/react-android-0.76.5-release/prefab/modules/reactnative/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
